e10c58c0ebac22a6361dbb9258e2b286155e7110
Unusual-Intellectual <leshalitova@gmail.com>
2022-02-14 22:41:19 +0200

    Initial commit

---
+++ src/test/java/com/example/shopjava/ShopJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
