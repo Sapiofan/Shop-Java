d366e1f1c6b3a0082b552e5a0c5c31eb5c68bef0
Unusual-Intellectual <leshalitova@gmail.com>
2022-02-24 22:39:56 +0200

    frontend 1.0

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -9,3 +9,38 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Get the modal
+var reg = document.getElementById('registration');
+var log = document.getElementById('login');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == reg) {
+        modal.style.display = "none";
+    }
+    if (event.target == log) {
+        modal.style.display = "none";
+    }
+}
+
+const buttonsWrapper = document.querySelector(".map");
+const slides = document.querySelector(".inner");
+
+buttonsWrapper.addEventListener("click", e => {
+    if (e.target.nodeName === "BUTTON") {
+        Array.from(buttonsWrapper.children).forEach(item =>
+            item.classList.remove("active")
+        );
+        if (e.target.classList.contains("first")) {
+            slides.style.transform = "translateX(-0%)";
+            e.target.classList.add("active");
+        } else if (e.target.classList.contains("second")) {
+            slides.style.transform = "translateX(-33.33333333333333%)";
+            e.target.classList.add("active");
+        } else if (e.target.classList.contains('third')){
+            slides.style.transform = 'translatex(-66.6666666667%)';
+            e.target.classList.add('active');
+        }
+    }
+});
