8eb3e643bcf3ac64c2b33f6455b60d648f387e85
Unusual-Intellectual <leshalitova@gmail.com>
2022-03-20 14:18:06 +0200

    frontend 2.0

--- src/main/resources/static/js/description.js
+++ src/main/resources/static/js/description.js
@@ -1,12 +1,3 @@
-//   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//         e.preventDefault();
-
-//         document.querySelector(this.getAttribute('href')).scrollIntoView({
-//             behavior: 'smooth'
-//         });
-//     });
-// });
 
 // Get all sections that have an ID defined
 // const sections = document.querySelectorAll("section[id]");
@@ -55,15 +46,41 @@
 //   });
 // }
 
-window.onscroll = function() {myFunction()};
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".chars-nav a");
+
+window.onscroll = function() {fixHeader()};
 
 var navbar = document.getElementById("chars-header");
 var sticky = navbar.offsetTop;
+var but = document.getElementById("header-buy");
+var stickyBut = but.offsetTop;
 
-function myFunction() {
+function fixHeader() {
     if (window.pageYOffset >= sticky) {
-        navbar.classList.add("fix")
+        navbar.classList.add("fix");
     } else {
         navbar.classList.remove("fix");
     }
+    if (window.pageYOffset >= 250) {
+        navbar.classList.add("fix-but");
+    } else {
+        navbar.classList.remove("fix-but");
+    }
+
+    var current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (window.pageYOffset >= sectionTop ) {
+            current = section.getAttribute("id"); }
+    });
+
+    navLi.forEach((a) => {
+        a.classList.remove("active-navigation");
+        if (a.classList.contains(current)) {
+            a.classList.add("active-navigation");
+        }
+    });
 }
